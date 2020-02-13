bc4566bd11aa35b105c13b1f4857dea8a72e9f12
Ryosuke <ryosuke.san.hana@gmail.com>
2020-02-12 17:57:07 -0800

    :boom: hiding Github contributors

--- src/components/doc-component/doc-component.tsx
+++ src/components/doc-component/doc-component.tsx
@@ -1,22 +1,34 @@
-import { Component, Host, Prop, ComponentInterface, h, Build } from '@stencil/core';
-import siteStructure from '../../assets/docs-structure.json';
-import { findItem, fileNotFound, ItemInfo } from '../../global/site-structure-utils';
-import { MarkdownContent, SiteStructureItem } from '../../global/definitions';
-
+import {
+  Component,
+  Host,
+  Prop,
+  ComponentInterface,
+  h,
+  Build
+} from "@stencil/core";
+import siteStructure from "../../assets/docs-structure.json";
+import {
+  findItem,
+  fileNotFound,
+  ItemInfo
+} from "../../global/site-structure-utils";
+import { MarkdownContent, SiteStructureItem } from "../../global/definitions";
 
 @Component({
-  tag: 'doc-component',
-  styleUrl: 'doc-component.css'
+  tag: "doc-component",
+  styleUrl: "doc-component.css"
 })
 export class DocumentComponent implements ComponentInterface {
-
   @Prop() page?: string;
   data?: ItemInfo;
   content?: MarkdownContent;
 
   async componentWillRender() {
     if (this.page) {
-      const data = this.data = findItem(siteStructure as SiteStructureItem[], this.page);
+      const data = (this.data = findItem(
+        siteStructure as SiteStructureItem[],
+        this.page
+      ));
 
       if (!Build.isBrowser && !data.item) {
         fileNotFound();
@@ -39,13 +51,21 @@ export class DocumentComponent implements ComponentInterface {
       <Host>
         <div class="container">
           <div class="container-inner">
-            <stencil-route-title pageTitle={content.title}></stencil-route-title>
+            <stencil-route-title
+              pageTitle={content.title}
+            ></stencil-route-title>
             <app-burger />
-            <site-menu selectedParent={data.parent} siteStructureList={siteStructure as SiteStructureItem[]} />
+            <site-menu
+              selectedParent={data.parent}
+              siteStructureList={siteStructure as SiteStructureItem[]}
+            />
             <div class="doc-content measure-lg">
               {toHypertext(content.hypertext)}
-              <lower-content-nav next={data.nextItem} prev={data.prevItem}></lower-content-nav>
-              <contributor-list contributors={content.contributors}></contributor-list>
+              <lower-content-nav
+                next={data.nextItem}
+                prev={data.prevItem}
+              ></lower-content-nav>
+              {/* <contributor-list contributors={content.contributors}></contributor-list> */}
             </div>
             <in-page-navigation
               pageLinks={content.headings}
@@ -59,41 +79,41 @@ export class DocumentComponent implements ComponentInterface {
   }
 }
 
-
 const localCache = new Map<string, Promise<MarkdownContent>>();
 
 const fetchContent = (path: string) => {
-
   let promise = localCache.get(path);
   if (!promise) {
-    console.log('fetchContent', path);
+    console.log("fetchContent", path);
     promise = fetch(path).then(response => response.json());
     localCache.set(path, promise);
   }
   return promise;
-}
+};
 
 const toHypertext = (data: any) => {
   if (!Array.isArray(data)) {
-    console.error('content error, hypertext is undefined')
+    console.error("content error, hypertext is undefined");
     return null;
   }
   const args = [];
   for (let i = 0; i < data.length; i++) {
     let arg = data[i];
-    if (i === 0 && typeof arg === 'string' && tagBlacklist.includes(arg.toLowerCase().trim())) {
-      arg = 'template';
-
+    if (
+      i === 0 &&
+      typeof arg === "string" &&
+      tagBlacklist.includes(arg.toLowerCase().trim())
+    ) {
+      arg = "template";
     } else if (i === 1 && arg) {
       const attrs: any = {};
       Object.keys(arg).forEach(key => {
         const k = key.toLowerCase();
-        if (!k.startsWith('on') && k !== 'innerhtml') {
+        if (!k.startsWith("on") && k !== "innerhtml") {
           attrs[key] = arg[key];
         }
       });
       arg = attrs;
-
     } else if (i > 1) {
       if (Array.isArray(arg)) {
         arg = toHypertext(arg);
@@ -104,4 +124,12 @@ const toHypertext = (data: any) => {
   return (h as any).apply(null, args);
 };
 
-const tagBlacklist = ['script', 'link', 'meta', 'object', 'head', 'html', 'body'];
+const tagBlacklist = [
+  "script",
+  "link",
+  "meta",
+  "object",
+  "head",
+  "html",
+  "body"
+];
